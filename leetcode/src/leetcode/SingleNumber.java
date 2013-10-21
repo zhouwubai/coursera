1a8fea3c0896b944e92f604cf4289fc4e5332b94
Wubai Zhou <zhouwubai@gmail.com>
2013-10-20 23:54:02 -0400

    add single number

---
+++ leetcode/src/leetcode/SingleNumber.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int rtn = 0;
+        for(int i = 0; i < A.length; i++) {
+            rtn ^= A[i];
+        }
+        
+        return rtn;
+    }
+	
+	
+}
