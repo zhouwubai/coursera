4e9143147f66ea5a90b0c5594d2ceb46299a79e5
Wubai Zhou <zhouwubai@gmail.com>
2013-10-21 23:23:07 -0400

    add climbStairs

---
+++ leetcode/src/leetcode/ClimbStairs.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class ClimbStairs {
+
+	// this is very computationaly expensive
+	public int climbStairsOld(int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if(n <= 2) return n;
+        return climbStairsOld(n-1) + climbStairsOld(n-2);
+    }
+	
+	public int climbStairs(int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if(n <= 2) return n;
+        int[] steps = new int[n];
+        steps[0] = 1;
+        steps[1] = 2;
+        for(int i = 2; i < n; i ++) {
+            steps[i] = steps[i-1] + steps[i-2];
+        }
+        return steps[n-1];
+    }
+}
